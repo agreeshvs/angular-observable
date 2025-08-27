f9e4c36b1c3edbc947d2f788ac42c17916c64f5e
agreeshvs <agreesh777@gmail.com>
2025-08-27 12:28:03 +0530

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,15 +30,28 @@ export class AppComponent {
     setTimeout(() => {
       observer.next(3);
     }, 3000);
-    setTimeout(() => {
+   /*  setTimeout(() => {
       observer.error(new Error('Something went wrong please try later.'));
-    }, 3000);
+    }, 3000); */
+    // After error, other subscribe functions will not work.
+    // Once error is emitted, no data or complete signal will not emitted.
+
     setTimeout(() => {
       observer.next(4);
     }, 4000);
     setTimeout(() => {
       observer.next(5);
     }, 5000);
+    
+
+    // Complete method - signals completion of data stream
+
+    setTimeout(() => {
+      observer.complete();
+    }, 6000);
+
+    // data stream will complete after 3 seconds.
+    // Other data streams will not work
   });
   
 
@@ -46,11 +59,30 @@ export class AppComponent {
     // 2. Subscribe to the Observable
     // Observer
     // next, error, complete are callback functions
-    this.myObservable.subscribe( (val:any) => {
+    /* this.myObservable.subscribe( (val:any) => {
       // Handler for each emitted value
       this.data.push(val);
-    }, error => {
-      console.error('Error occurred: ', error);
-    });
+    }, (err) => {
+      // Call back function to handle error events
+      alert(err.message);
+    }, () =>{
+      // callback function for complete event.
+      alert('Data stream ended.');
+    }); */
+
+
+
+    this.myObservable.subscribe({
+      next:( val: any) =>{
+        this.data.push(val)
+      },
+      error(err){
+        alert(err.message);
+      },
+      complete(){
+        alert('Complete message');
+      }
+    })
+
   }
 }
