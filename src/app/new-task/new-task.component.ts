ce8d1668d14b29dd54b6b673cc2fea1c773f3702
agreeshvs <agreesh777@gmail.com>
2025-08-27 21:05:09 +0530

    Subjects in RxJS

---
+++ src/app/new-task/new-task.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { TaskService } from '../Services/task.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrls: ['./new-task.component.css']
+})
+export class NewTaskComponent {
+  newTask: string = '';
+  taskService: TaskService = inject(TaskService);
+
+  createnewTask(){
+    console.log(this.newTask);
+    this.taskService.onCreateTask(this.newTask);
+
+  }
+}
